f8670adf5fa8a1e5cf5fa469ccfc747109b23249
husni47 <husnithecitizen@gmail.com>
2024-03-18 21:27:58 +0700

    testing upload foto

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
           </div>
           <!-- /.col -->
         </div>
